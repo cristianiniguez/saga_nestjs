61fd9fad79ac4a9156946f6daed3dede4edbc052
cristianiniguez <felizpc1@gmail.com>
2021-12-05 21:26:59 -0400

    modulos de nestjs: useFactory

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,7 @@
 import { Module } from '@nestjs/common';
+import { HttpModule, HttpService } from '@nestjs/axios';
 import { ConfigModule } from '@nestjs/config';
+import { firstValueFrom } from 'rxjs';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -7,7 +9,7 @@ import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), UsersModule, ProductsModule],
+  imports: [ConfigModule.forRoot(), HttpModule, UsersModule, ProductsModule],
   controllers: [AppController],
   providers: [
     AppService,
@@ -15,6 +17,16 @@ import { ProductsModule } from './products/products.module';
       provide: 'API_KEY',
       useValue:
         process.env.NODE_ENV === 'production' ? 'QWERTYUIO' : '123456789',
+    },
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) => {
+        const tasks = await firstValueFrom(
+          http.get('https://jsonplaceholder.typicode.com/todos'),
+        );
+        return tasks.data;
+      },
+      inject: [HttpService],
     },
   ],
 })
