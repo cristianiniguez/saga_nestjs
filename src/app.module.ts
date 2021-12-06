2e83390c70a4d4f175da1b56fa99f3e8f838bc6b
cristianiniguez <felizpc1@gmail.com>
2021-12-05 20:35:17 -0400

    modulos de nestjs: useValue y useClass

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [UsersModule, ProductsModule],
+  imports: [ConfigModule.forRoot(), UsersModule, ProductsModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: 'API_KEY',
+      useValue:
+        process.env.NODE_ENV === 'production' ? 'QWERTYUIO' : '123456789',
+    },
+  ],
 })
 export class AppModule {}
