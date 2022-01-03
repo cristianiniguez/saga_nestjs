328187c6dd2cf771d9d40f0a22b94e70481ab006
cristianiniguez <felizpc1@gmail.com>
2022-01-02 21:19:58 -0400

    mongoose: implementando mongoose en modulos

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,4 +1,7 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { Product, ProductSchema } from './entities/product.entity';
 
 import { ProductsController } from './controllers/products.controller';
 import { CategoriesController } from './controllers/categories.controller';
@@ -9,6 +12,9 @@ import { CategoriesService } from './services/categories.service';
 import { BrandsService } from './services/brands.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Product.name, schema: ProductSchema }]),
+  ],
   controllers: [ProductsController, CategoriesController, BrandsController],
   providers: [ProductsService, CategoriesService, BrandsService],
   exports: [ProductsService],
