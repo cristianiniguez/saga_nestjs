02da7876aee86d7090ab64d4bd35b26f42fe11bc
cristianiniguez <felizpc1@gmail.com>
2022-01-07 20:54:49 -0400

    database: conexion como injectable y ejecutando un select

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
 import { ConfigType } from '@nestjs/config';
+import { Client } from 'pg';
 
 import config from './config';
 
@@ -7,11 +8,20 @@ import config from './config';
 export class AppService {
   constructor(
     @Inject(config.KEY) private configService: ConfigType<typeof config>,
+    @Inject('PG') private pgClient: Client,
   ) {}
 
   getHello(): string {
     const apiKey = this.configService.apiKey;
     const dbName = this.configService.database.name;
     return `Hello World! ${apiKey} ${dbName}`;
+  }
+
+  getTasks() {
+    return new Promise((resolve, reject) => {
+      this.pgClient.query('SELECT * FROM tasks', (err, res) => {
+        err ? reject(err) : resolve(res.rows);
+      });
+    });
   }
 }
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,4 +1,14 @@
 import { Global, Module } from '@nestjs/common';
+import { Client } from 'pg';
+
+const client = new Client({
+  user: 'root',
+  host: 'localhost',
+  database: 'my_db',
+  password: '123456',
+  port: 5432,
+});
+client.connect();
 
 @Global()
 @Module({
@@ -8,7 +18,11 @@ import { Global, Module } from '@nestjs/common';
       useValue:
         process.env.NODE_ENV === 'production' ? 'QWERTYUIP' : '123456789',
     },
+    {
+      provide: 'PG',
+      useValue: client,
+    },
   ],
-  exports: ['API_KEY'],
+  exports: ['API_KEY', 'PG'],
 })
 export class DatabaseModule {}
