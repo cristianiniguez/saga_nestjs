7330e7348260c0cb5aa4051b385811687ee2e9ed
cristianiniguez <felizpc1@gmail.com>
2022-02-20 14:18:54 -0400

    usando un decorador

--- src/auth/guards/api-key.guard.ts
+++ src/auth/guards/api-key.guard.ts
@@ -1,3 +1,4 @@
+import { Reflector } from '@nestjs/core';
 import {
   CanActivate,
   ExecutionContext,
@@ -6,12 +7,22 @@ import {
 } from '@nestjs/common';
 import { Request } from 'express';
 import { Observable } from 'rxjs';
+import { IS_PUBLIC } from '../decorators/public.decorator';
 
 @Injectable()
 export class ApiKeyGuard implements CanActivate {
+  constructor(private readonly reflector: Reflector) {}
+
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+    const isPublic = this.reflector.get<boolean>(
+      IS_PUBLIC,
+      context.getHandler(),
+    );
+
+    if (isPublic) return true;
+
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.header('Auth');
     const isAuth = authHeader === '1234';
