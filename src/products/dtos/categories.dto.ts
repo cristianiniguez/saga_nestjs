20e393f23835929f1d6eb2e37a716ad19c875493
cristianiniguez <felizpc1@gmail.com>
2022-02-20 11:54:37 -0400

    introduccion: instalacion de platzistore

--- src/products/dtos/categories.dto.ts
+++ src/products/dtos/categories.dto.ts
@@ -1,14 +1,10 @@
-import { IsString, IsNotEmpty, IsUrl } from 'class-validator';
+import { IsString, IsNotEmpty } from 'class-validator';
 import { PartialType } from '@nestjs/swagger';
 
 export class CreateCategoryDTO {
   @IsString()
   @IsNotEmpty()
   readonly name: string;
-
-  @IsUrl()
-  @IsNotEmpty()
-  readonly image: string;
 }
 
 export class UpdateCategoryDTO extends PartialType(CreateCategoryDTO) {}
