a428fb0c9680abe14d601e66a096d7a9614d3c39
cristianiniguez <felizpc1@gmail.com>
2022-03-06 16:20:17 -0400

    autenticacion con passport: ruta de login

---
+++ src/auth/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+
+import { UsersService } from 'src/users/services/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private usersService: UsersService) {}
+
+  async validateUser(email: string, password: string) {
+    const user = await this.usersService.findByEmail(email);
+    if (!user) return null;
+
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) return null;
+
+    const { password: _, ...rta } = user.toJSON();
+    return rta;
+  }
+}
